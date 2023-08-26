c0912ec7a7752ccae1375b599d0f67bd7a8c95f1
brunoBMonteiro <brunobilheri@hotmail.com>
2023-08-26 14:24:47 -0300

    Variables

--- challengesPy/introduction.py
+++ challengesPy/introduction.py
@@ -23,3 +23,25 @@ print(c)
 d = "Teste de texto"
 print(d)
 
+# variáveis
+
+# Formatação de string
+name = "Bruno"
+string_formatada = f"Bom dia, {name}!"
+print(string_formatada)
+
+# concatenação se string
+saudacao = "Olá "
+nome = "Bruno"
+nova_string = saudacao + nome
+print(nova_string)
+
+# Multiplicação de string
+string_base2 = "Teste "
+nova_string2 = string_base2*2
+print(nova_string2)
+
+# Substituição
+string_base3 = "Olá Maria"
+nova_string3 = string_base3.replace("Maria", "Ana")
+print(nova_string3)
