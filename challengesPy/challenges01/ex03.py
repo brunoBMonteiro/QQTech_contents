cb01cfa8eba036ac560fde1b8e8f40fc4a6dfa10
brunoBMonteiro <brunobilheri@hotmail.com>
2023-08-26 16:54:03 -0300

    Finished string manipulation

---
+++ challengesPy/challenges01/ex03.py
@@ -0,0 +1,5 @@
+list = [1,2,3,4,5]
+x = list[0]
+y = list[2]
+soma = x + y
+print(soma)
---
+++ challengesPy/challenges01/ex04.py
@@ -0,0 +1,7 @@
+value = '0123456789'
+
+print(value[2:10])
+print(value[0:5])
+print(value[2:6])
+print(value[-1:])
+print(value[-2])
